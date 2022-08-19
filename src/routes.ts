54e00eecb9a3ca1a1471040f00d305ed167e7567
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-08-19 09:37:20 -0300

    routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateUserController } from "./controllers/CreateUserController";
 
 const routes = Router();
 
 const createUserController = new CreateUserController();
+const authenticateUserController = new AuthenticateUserController();
 
 routes.post('/users/create', createUserController.handle);
+routes.post('/session', authenticateUserController.handle);
 
 export { routes };
