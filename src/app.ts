2e263c488a200267d554faf54a9289e60e30f329
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-08-19 09:36:54 -0300

    error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import { AppError } from './error/AppError';
 import { routes } from './routes';
 
 const app = express();
@@ -8,7 +9,15 @@ app.use(express.json());
 app.use(routes);
 
 app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
 
+  console.error(error);
+  return response.sendStatus(500);
 });
 
 export { app };
