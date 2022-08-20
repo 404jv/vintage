e282b908765f2736a94c40fdd6eccd4e46ec74d1
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-08-20 11:18:02 -0300

    create route to get last 40 messages

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateUserController } from "./controllers/CreateUserController";
+import { GetLast40MessagesController } from "./controllers/GetLast40MessagesController";
 import { SendMessageController } from "./controllers/SendMessageController";
 import { ensureAuthenticated } from "./middlewares/ensureAuthenticated";
 
@@ -9,10 +10,12 @@ const routes = Router();
 const createUserController = new CreateUserController();
 const authenticateUserController = new AuthenticateUserController();
 const sendMessageController = new SendMessageController();
+const getLast40MessagesController = new GetLast40MessagesController();
 
 routes.post('/users/create', createUserController.handle);
 routes.post('/session', authenticateUserController.handle);
 
 routes.post('/messages/create', ensureAuthenticated, sendMessageController.handle);
+routes.get('/messages', ensureAuthenticated, getLast40MessagesController.handle);
 
 export { routes };
