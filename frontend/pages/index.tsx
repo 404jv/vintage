8ef62287a712368e13f11fc6757713f2a87954fe
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-08-21 11:34:20 -0300

    do not show home if user has no token

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from 'next'
+import { useContext } from 'react';
 import { Chat } from '../components/Chat';
 import { UsersOnline } from '../components/UsersOnline';
+import { AuthContext } from '../contexts/auth';
 import { socket } from '../services/socket';
 
 import styles from '../styles/Home.module.css';
@@ -8,11 +10,17 @@ import styles from '../styles/Home.module.css';
 socket.emit('connection');
 
 const Home: NextPage = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className={styles.container}>
-      <UsersOnline />
+      {user && (
+        <>
+          <UsersOnline />
 
-      <Chat />
+          <Chat />
+        </>
+      )}
     </div>
   )
 }
