b62bb523f22e6cc51cf0c18b8ce65f836b448e9d
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-08-22 08:48:23 -0300

    test

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-console.log(process.env.BASE_URL);
 console.log('process.env.BASE_URL');
+console.log(process.env.BASE_URL);
 
 export const api = axios.create({
   baseURL: process.env.BASE_URL || 'http://localhost:3000',
