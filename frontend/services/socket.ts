628a36539df370922de04736c94d1101a8ff2a5e
João Victor Ramalho Alves <joaovictorramalho7@gmail.com>
2022-08-22 08:52:42 -0300

    rename variable with the prefix NEXT_PUBLIC

--- frontend/services/socket.ts
+++ frontend/services/socket.ts
@@ -1,5 +1,5 @@
 import io from 'socket.io-client';
 
-const socket = io(process.env.BASE_URL || 'http://localhost:3000');
+const socket = io(process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000');
 
 export { socket };
